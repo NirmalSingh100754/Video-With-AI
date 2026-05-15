412da439c6037c5d5e2f8ee368b207d00b31ae98
NirmalSingh100754 <nirmalsingh100754@gmail.com>
2026-05-16 00:22:21 +0530

    feat: implement POST endpoint for authenticated video creation in video route

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,9 @@
+import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
-import Video from "@/models/Video";
-import { NextResponse } from "next/server";
+import Video, { Ivideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { New_Amsterdam } from "next/font/google";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
@@ -13,6 +16,47 @@ export async function GET() {
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch videos" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized!" }, { status: 401 });
+    }
+    await connectToDatabase();
+    const body: Ivideo = await request.json();
+    if (
+      !body.title ||
+      !body.description ||
+      !body.videoUrl ||
+      !body.thumbnailUrl
+    ) {
+      return NextResponse.json(
+        { error: "Missing required fields." },
+        { status: 400 },
+      );
+    }
+    const videoData = {
+      ...body,
+      controls: body?.controls ?? true,
+      transformation: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality ?? 100,
+      },
+    };
+    const newVideo = await Video.create(videoData);
+    return NextResponse.json(
+      { message: "Video Record created successfully.", video: newVideo },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create video Record." },
       { status: 500 },
     );
   }
